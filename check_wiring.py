7c1f66933e16d6789d9a6dfeacf39c3eef72b85b
Gabriel Vandersippe <gabriel.vandersippe@etu.minesparis.psl.eu>
2025-07-04 14:39:57 +0200

    Documantation, début suppression images

--- check_wiring.py
+++ check_wiring.py
@@ -1,6 +1,16 @@
 from Wiring_Checks.wire import analyseWires
 import sys
 
+"""
+This file is used to run checks on an image, namely the number of wires and whether or not these wires are in contact.
+
+Arguments
+-----------
+path - str : the path to the image to analyse.
+
+Returns : the wire count next to the theoretical one ; creates an image highlighting the touching wires.
+"""
+
 if __name__ == '__main__' :
     path = sys.argv[1]
     analyseWires(path)
